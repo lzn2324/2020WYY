a23559798df27c32f8d983662c01f5ac8dc05626
xuyuan <1014189389@qq.com>
2020-11-20 19:19:05 +0800

    登录

---
+++ 徐源/wyy2/wyy2.js
@@ -0,0 +1,21 @@
+Page({
+  // 页面的初始数据
+  data: {
+    inputShowed: false,  //初始文本框不显示内容
+    
+  },
+  // 使文本框进入可编辑状态
+  showInput: function () {
+    this.setData({
+      inputShowed: true   //设置文本框可以输入内容
+    });
+  },
+  // 取消搜索
+  hideInput: function () {
+    this.setData({
+      inputShowed: false
+    });
+  }
+  
+
+});
